5f00e4b4eb84305b66acf1ecb43e154267489185
retcon1 <benlucascoding@outlook.com>
2024-02-05 19:23:20 +0000

    feat: adds another removeContact test

--- src/test/java/addressbook/app/AddressBookTest.java
+++ src/test/java/addressbook/app/AddressBookTest.java
@@ -70,17 +70,36 @@ public class AddressBookTest {
 
         @Test
         public void removeContactReducesArraySizeByOne() {
+            // Arrange
             Contact mockContact = mock(Contact.class);
             when(mockContact.getName()).thenReturn("Test Name");
             when(mockContact.getNumber()).thenReturn("12345678910");
             when(mockContact.getEmail()).thenReturn("test@email.com");
             testBook.addContact(mockContact);
-            System.out.println(testBook.getContacts().size());
             // Act
             testBook.removeContact(mockContact);
             // Assert
-            System.out.println(testBook.getContacts().size());
             assertEquals(0, testBook.getContacts().size());
+        }
+
+        @Test
+        public void removeContactRemovesTheExactContact() {
+            Contact mockContact = mock(Contact.class);
+            when(mockContact.getName()).thenReturn("Test Name");
+            when(mockContact.getNumber()).thenReturn("12345678910");
+            when(mockContact.getEmail()).thenReturn("test@email.com");
+
+            Contact mockContact2 = mock(Contact.class);
+            when(mockContact2.getName()).thenReturn("Another Name");
+            when(mockContact2.getNumber()).thenReturn("12345678444");
+            when(mockContact2.getEmail()).thenReturn("test2@email.com");
+            testBook.addContact(mockContact);
+            testBook.addContact(mockContact2);
+            // Act
+            testBook.removeContact(mockContact);
+            String result = testBook.getContacts().get(0).getName();
+            // Assert
+            assertEquals(mockContact2.getName(), result);
         }
     }
 }
