fa93c7385aa7135540c2f26862aa69de3154a67f
retcon1 <benlucascoding@outlook.com>
2024-02-05 18:44:11 +0000

    feat: adds tests for addContact function, which also tests getContacts, adds code to pass

--- src/main/java/addressbook/app/AddressBook.java
+++ src/main/java/addressbook/app/AddressBook.java
@@ -1,14 +1,26 @@
 package addressbook.app;
 
+import java.util.ArrayList;
+
 public class AddressBook {
     private int id;
     private static int nextId = 1000;
+    private ArrayList<Contact> contacts;
 
     AddressBook() {
         this.id = AddressBook.nextId++;
+        this.contacts = new ArrayList<>();
     }
 
     public int getId() {
         return id;
+    }
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
     }
 }
--- src/test/java/addressbook/app/AddressBookTest.java
+++ src/test/java/addressbook/app/AddressBookTest.java
@@ -1,23 +1,67 @@
 package addressbook.app;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.mockito.Mockito.*;
 
 public class AddressBookTest {
+    private AddressBook testBook;
+
+    @BeforeEach
+    void newBook() {
+        testBook = new AddressBook();
+    }
     @Nested
     @DisplayName("AddressBook class exists with appropriate ID")
     class AddressBookExistsWithId {
         @Test
         public void addressBookExistsWithId() {
-            // Arrange
-            AddressBook testBook = new AddressBook();
             // Assert
             assertInstanceOf(AddressBook.class, testBook);
-            assertEquals( 1000, testBook.getId());
+            assertInstanceOf(Integer.class, testBook.getId());
+        }
+    }
+
+    @Nested
+    @DisplayName("addContact function tests")
+    class AddContactsFunctionTests {
+
+        @Test
+        public void addContactIncreasesArrayListSizeByOne() {
+            // Arrange
+            Contact mockContact = mock(Contact.class);
+            when(mockContact.getName()).thenReturn("Test Name");
+            when(mockContact.getNumber()).thenReturn("12345678910");
+            when(mockContact.getEmail()).thenReturn("test@email.com");
+
+            // Act
+            testBook.addContact(mockContact);
+            int result = testBook.getContacts().size();
+
+            // Assert
+            assertEquals(1, result);
+        }
+
+        @Test
+        public void addContactPutsInstanceOfContactIntoArrayList() {
+            // Arrange
+            Contact mockContact = mock(Contact.class);
+            when(mockContact.getName()).thenReturn("Test Name");
+            when(mockContact.getNumber()).thenReturn("12345678910");
+            when(mockContact.getEmail()).thenReturn("test@email.com");
+
+            // Act
+            testBook.addContact(mockContact);
+            Contact result = testBook.getContacts().get(0);
+
+            // Assert
+            assertEquals(mockContact, result);
+            assertEquals(mockContact.getName(), result.getName());
         }
     }
 }
