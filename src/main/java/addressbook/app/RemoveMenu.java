8e7850e33d3949927cc41493bb1b9dc4bc9f0615
retcon1 <benlucascoding@outlook.com>
2024-02-10 11:18:18 +0000

    feat: creates remove menu and tests for it

--- src/main/java/addressbook/app/RemoveMenu.java
+++ src/main/java/addressbook/app/RemoveMenu.java
@@ -1,2 +1,33 @@
-package addressbook.app;public class RemoveMenu {
+package addressbook.app;
+
+import static addressbook.app.App.input;
+import static addressbook.app.App.myAddressBook;
+
+public class RemoveMenu {
+    public static void removeContact() {
+        System.out.println("Please enter the number of the contact you'd like to remove...");
+        Printer.printContacts(myAddressBook.getContacts());
+        String indexToRemove = input.nextLine();
+        try {
+            Contact contactToRemove = ContactsViewerMenu.selectContact(indexToRemove);
+            myAddressBook.removeContact(contactToRemove);
+            System.out.println("Contact removed!");
+            Printer.printContacts(myAddressBook.getContacts());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Select a contact with an index in the list next time!");
+        } finally {
+            MenuOptions.printMainMenu();
+        }
+    }
+
+    public static void removeAllContacts() {
+        System.out.println("Are you sure you wish to wipe all of your contacts? This cannot be undone\n Type: yes to remove everyone, type anything else to abort");
+        String decision = input.nextLine();
+        System.out.println(decision);
+        if (decision.equals("yes")) {
+            myAddressBook.removeAllContacts();
+            System.out.println("ALL CONTACTS DELETED");
+        }
+        MenuOptions.printMainMenu();
+    }
 }
--- src/test/java/addressbook/app/RemoveMenuTest.java
+++ src/test/java/addressbook/app/RemoveMenuTest.java
@@ -1,2 +1,85 @@
-package addressbook.app;public class RemoveMenuTest {
+package addressbook.app;
+
+
+
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class RemoveMenuTest {
+
+    private final InputStream originalInput = System.in;
+    private final RemoveMenu removeMenu = new RemoveMenu();
+
+    @BeforeEach
+    public void setUp() {
+        App.myAddressBook = new AddressBook(); // Create a new AddressBook before each test
+        Contact demoContact = new Contact("Demo Contact", "09871234653", "demo@contact.com");
+        Contact demoContact2 = new Contact("Another Demo", "12350098734", "contact@demo.co.uk");
+        App.myAddressBook.addContact(demoContact);
+        App.myAddressBook.addContact(demoContact2);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setIn(originalInput); // Restore original input stream after each test
+    }
+
+
+    @Nested
+    @DisplayName("remove single contact functionality")
+    class RemoveSingleContactFunction {
+        @Test
+        public void testRemoveContactSuccessful() {
+            String input = "0\n7";
+            provideInput(input);
+            removeMenu.removeContact();
+            assertEquals(1, App.myAddressBook.getContacts().size());
+            assertEquals("Another Demo", App.myAddressBook.getContacts().get(0).getName());
+        }
+
+        @Test
+        public void testRemoveContactFailsWithInvalidIndex() {
+            String input = "8\n7";
+            provideInput(input);
+            removeMenu.removeContact();
+            assertEquals(2, App.myAddressBook.getContacts().size());
+        }
+    }
+
+    @Nested
+    @DisplayName("remove all contacts functionality")
+    class RemoveAllContactsFunction {
+        @Test
+        public void testRemoveAllContactsSuccessful() {
+            String input = "yes\n7";
+            provideInput(input);
+            removeMenu.removeAllContacts();
+            assertEquals(0, App.myAddressBook.getContacts().size());
+        }
+
+        @Test
+        public void testRemoveAllContactsUnsuccessful() {
+            String input = "no\n7";
+            provideInput(input);
+            removeMenu.removeAllContacts();
+            assertEquals(2, App.myAddressBook.getContacts().size());
+        }
+    }
+
+    private void provideInput(String data) {
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Scanner scanner = new Scanner(System.in);
+        try {
+            App.setInput(scanner);
+        } finally {
+            System.setIn(stdin); // Reset System.in to original stream
+        }
+    }
 }
